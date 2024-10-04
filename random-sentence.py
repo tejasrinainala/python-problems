968a98d638e0dd6e6f6400d3885ae66c9ffa6794
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-10-04 20:55:42 +0530

    Create random-sentence.py

---
+++ random-sentence.py
@@ -0,0 +1,7 @@
+import random
+nouns = ["cat", "dog", "house", "car", "tree"]
+verbs = ["runs", "jumps", "sleeps", "drives", "eats"]
+adjectives = ["big", "small", "fast", "slow", "green"]
+adverbs = ["quickly", "quietly", "happily", "slowly", "loudly"]
+print(F"Random sentence:{nouns[random.randint(0,len(nouns)-1)]} {verbs[random.randint(0,len(verbs)-1)]} {adjectives[random.randint(0,len(adjectives)-1)]} {adverbs[random.randint(0,len(adverbs)-1)]}")
+
