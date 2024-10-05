32d2ed8f12191b362846bb9043cad48b96c3243b
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-10-05 19:32:33 +0530

    adding description to no-of-heads&tails.py

--- no-of-heads&tails.py
+++ no-of-heads&tails.py
@@ -1,3 +1,14 @@
+# Random Coin Flip Simulator
+
+# Description:
+# This program simulates flipping a coin a specified number of times and counts the number of heads and tails.
+
+
+
+
+
+
+
 import random
 n=int(input("Enter no of times you want to flip the coin : "))
 heads=random.randint(1,n)
