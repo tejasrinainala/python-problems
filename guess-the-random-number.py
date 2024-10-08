d64a40c543cfcd54de83cddea16f223313773914
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-10-08 17:48:56 +0530

    adding the solution to  guess-the-random-number.py

--- guess-the-random-number.py
+++ guess-the-random-number.py
@@ -1,1 +1,15 @@
+import random
+n=random.randint(0,20)
+i=0
+while True:
+        m=int(input("Enter a number : "))
+        i+=1
+        if n>m:
+            print("The number is too low!")
+        elif n<m:
+            print("The number is too high!")
+        else:
+            print(F"Hurray! you guessed the number correctly in {i} attempts")
+            break
+            
 
