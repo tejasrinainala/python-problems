20df4fd5954db9c9730e77cd54030e793e27c58a
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-12-23 21:39:44 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,14 @@
+list1=[2,3,4,5,6]
+list2=[4,8,10,20,18]
+list3=[]
+i=0
+j=0
+while(i<len(list1)):
+    j=0
+    while(j<len(list2)):
+        if(list1[i]*2==list2[j]):
+            list3.append(list2[j])
+        j=j+1
+    i=i+1
+print(list3)
+            
