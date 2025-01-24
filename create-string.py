c2c2e7998409628a699cbd5e21cf2e22bd7d590c
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2025-01-24 20:00:42 +0530

    Create create-string.py

---
+++ create-string.py
@@ -0,0 +1,15 @@
+n=int(input())
+s=""
+for i in range(n):
+    asc=int(input("Enter ascii values: "))
+    s=s+chr(asc)
+
+print(s)
+
+
+# output:
+# 3
+# Enter ascii values: 67
+# Enter ascii values: 65
+# Enter ascii values: 82
+# CAR
