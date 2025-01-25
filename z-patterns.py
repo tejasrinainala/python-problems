61c0e30bed879db7c1c9ab49a84d6fad835a5097
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2025-01-25 20:12:27 +0530

    Create z-patterns.py

---
+++ z-patterns.py
@@ -0,0 +1,12 @@
+s="ZOHOCORPORATIONTEAM"
+n=7
+k=0
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i==n or i==1 or i+j==n+1:
+            print(s[k],end=" ")
+            k+=1
+        else:
+            print(" ",end=" ")
+    print()
+    
