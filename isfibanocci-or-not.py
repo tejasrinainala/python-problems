f75b86aaf06341c54d8a53d7c9fcc4fa7ded75c1
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2025-02-06 21:03:11 +0530

    Create isfibanocci-or-not.py

---
+++ isfibanocci-or-not.py
@@ -0,0 +1,12 @@
+def is_fibonacci(n):
+    if n < 0:
+        return False
+    n1, n2 = 0, 1
+    while n1 <= n:
+        if n1 == n:
+            return True
+        n1, n2 = n2, n1 + n2
+    return False
+
+n = int(input("Enter a number: "))
+print(is_fibonacci(n))
