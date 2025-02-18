8a0d5118c5ddf5fdd749770e385a7b56efc62dd7
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2025-02-18 19:58:08 +0530

    Update number-square-pattern.py

--- number-square-pattern.py
+++ number-square-pattern.py
@@ -7,3 +7,14 @@
 # 4 3 2 2 2 3 4
 # 4 3 3 3 3 3 4
 # 4 4 4 4 4 4 4
+
+
+
+n=int(input())
+size=2*n-1
+min_dis=0
+for i in range(size):
+    for j in range(size):
+        min_dis=min(i,j,size-1-i,size-1-j)
+        print(n-min_dis,end=" ")
+    print()
