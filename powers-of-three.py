93d62f6de9db35b91bd079fc99ff627998777a30
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2025-03-04 20:22:48 +0530

    Update powers-of-three.py

--- powers-of-three.py
+++ powers-of-three.py
@@ -1,3 +1,19 @@
+# Given an integer n, return true if it is possible to represent n as the sum of distinct powers of three. Otherwise, return false.
+
+# An integer y is a power of three if there exists an integer x such that y == 3x.
+
+ 
+
+# Example 1:
+
+# Input: n = 12
+# Output: true
+# Explanation: 12 = 31 + 32
+
+
+
+
+
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
         while n>0:
