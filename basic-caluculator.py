b9d4ceab2ff947dac1f32cfaeabe6fd58be66894
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2025-03-08 19:52:50 +0530

    Create basic-caluculator.py

---
+++ basic-caluculator.py
@@ -0,0 +1,32 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        num = 0
+        sign = 1  
+        result = 0
+
+        for i in range(len(s)):
+            char = s[i]
+
+            if char.isdigit():
+                num = num * 10 + int(char)  
+            
+            elif char in ('+', '-'):
+                result += sign * num
+                num = 0
+                sign = 1 if char == '+' else -1  
+            
+            elif char == '(':
+                stack.append(result)
+                stack.append(sign)
+                result = 0
+                sign = 1
+            
+            elif char == ')':
+                result += sign * num
+                num = 0
+                result *= stack.pop()  
+                result += stack.pop() 
+
+        return result + (sign * num)  
+
