ce1ca6924da08dcee27123e6b3cd5608d01efed2
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2025-03-17 20:13:20 +0530

    Update divide-array-into-equal-pairs.py

--- divide-array-into-equal-pairs.py
+++ divide-array-into-equal-pairs.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        nums.sort()  
+        for i in range(0, len(nums), 2):  
+            if nums[i] != nums[i + 1]:
+                return False
+        return True
