f23f37ed0bc18053f51afdf48bc2e3c0705bd860
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2025-04-07 19:56:15 +0530

    Create cap-small-pattern.py

---
+++ cap-small-pattern.py
@@ -0,0 +1,25 @@
+ # Alphabet pattern where for even column capital letters and odd column small letters should print.
+
+
+k1=65
+k2=98
+for i in range(5):
+    for j in range(i+1):
+        if i%2==0:
+            print(chr(k1),end=" ")
+            k1+=1
+            if i!=0:
+                k2+=2
+        else:
+            print(chr(k2),end=" ")
+            k2+=1
+            k1+=1
+    print()
+
+
+#output
+# A 
+# b c 
+# D E F 
+# j k l m 
+# K L M N O 
