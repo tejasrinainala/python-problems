3c8ddbaaee62f72f2c4a26a4c028691ec2b9d9ba
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2025-04-08 19:52:33 +0530

    Create rock-paper-scissors.py

---
+++ rock-paper-scissors.py
@@ -0,0 +1,18 @@
+import random
+choices=['rock','paper','scissors']
+comp=random.choice(choices)
+user=input().lower()
+if comp==user:
+    print("Draw")
+elif (comp=='rock' and user=='paper') or  (user=='rock' and comp=='scissors') or (comp=='paper' and user=='scissors'):
+    print("User won!")
+elif (user=='rock' and comp=='paper') or (comp=='rock' and user=='scissors') or (user=='paper' and comp=='scissors'):
+    print("Computer won!")
+else:
+    print("Enter valid input.")
+
+
+
+    
+
+    
